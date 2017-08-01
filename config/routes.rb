c13f66eaba7ec8b4fe3420764d006eabd40b02c3
Victor Wilm <victorrw@msn.com>
2017-08-01 16:22:58 -0300

    Basic articles structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  resources :articles
+
+=begin  get 'articles/index'
+
+  get 'articles/show'
+
+  get 'articles/new'
+
+  get 'articles/create'
+
+  get 'articles/edit'
+
+  get 'articles/update'
+
+  get 'articles/destroy'
+=end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
